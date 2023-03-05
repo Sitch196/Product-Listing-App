6af8caf967948e42bed49f996f31c2641114bb01
Sitch196 <sichinava1996@gmail.com>
2023-03-05 15:05:54 +0400

    sds d

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Spinner from "./Spinner";
+import styled from "styled-components";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getData = async function () {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const json = await response.json();
+        setData(json);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    const Delay = setTimeout(() => {
+      getData();
+    }, 2000);
+
+    return () => clearTimeout(Delay);
+  }, []);
+
+  return (
+    <div>
+      {loading && (
+        <SpinnerWrapper>
+          <Spinner />
+        </SpinnerWrapper>
+      )}
+      {data && (
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default App;
+const SpinnerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
